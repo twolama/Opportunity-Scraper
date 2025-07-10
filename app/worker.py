02c72eb1dcd5bfceb0bd460409f8447e1f97d222
mechatemesgen <ililnaafbarihe94@gmail.com>
2025-07-10 15:08:38 +0300

    new update v 0.1.2

--- app/worker.py
+++ app/worker.py
@@ -1,12 +1,12 @@
-import logging
-from app.scheduler import start_scheduler, run_daily_tasks
-from app.database import init_db
+# import logging
+# from app.scheduler import start_scheduler, run_daily_tasks
+# from app.database import init_db
 
-if __name__ == "__main__":
-    logging.basicConfig(
-        level=logging.INFO,
-        format='%(asctime)s - %(levelname)s - %(message)s'
-    )
-    init_db()
-    run_daily_tasks()  # optionally run once on start
-    start_scheduler()
+# if __name__ == "__main__":
+#     logging.basicConfig(
+#         level=logging.INFO,
+#         format='%(asctime)s - %(levelname)s - %(message)s'
+#     )
+#     init_db()
+#     run_daily_tasks()  # optionally run once on start
+#     start_scheduler()
