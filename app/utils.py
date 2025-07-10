139217552894834e2284ecf462c3686565959476
mechatemesgen <ililnaafbarihe94@gmail.com>
2025-07-10 19:53:20 +0300

    Version 0.1.6

--- app/utils.py
+++ app/utils.py
@@ -3,7 +3,7 @@ def format_telegram_message(opportunity: dict) -> str:
     description = opportunity.get("description", "")
     deadline = f"\n\n<b>Deadline:</b> {opportunity['deadline']}" if opportunity.get("deadline") else ""
     # apply_link = f'\n\n📨 <a href="{opportunity["link"]}"><b>Apply Now</b></a>' if opportunity.get("link") else ""
-    join_us = '\n\n✅ <a href="https://t.me/Scholarship_Spot"><b>Join Us</b></a>'
-    tags = "\n\n#Opportunities #Scholarships #Grants #Education #Career"
+    join_us = '\n\n✅ <a href="https://t.me/ScholarshipSpot"><b>Join Us</b></a>'
+    tags = "\n\n#Opportunities #Scholarships #Grants #Education #Career @ScholarshipSpot"
 
     return f"{title}\n\n{description}{deadline}{join_us}{tags}"
