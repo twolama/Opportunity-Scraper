bba2c970cbd8c9f097ade609a53b534f6cdddeb9
mechatemesgen <ililnaafbarihe94@gmail.com>
2025-07-10 13:44:30 +0300

    Deployment Ready V0.1.0

--- app/scheduler.py
+++ app/scheduler.py
@@ -5,7 +5,7 @@ from app.telegram_bot import post_new_opportunities
 from app.database import delete_old_entries
 
 def run_daily_tasks():
-    fetch_opportunities_by_date()  # use yesterday by default
+    fetch_opportunities_by_date()
     post_new_opportunities()
     delete_old_entries()
 
@@ -13,8 +13,6 @@ def start_scheduler():
     schedule.every().day.at("08:00").do(run_daily_tasks)
     schedule.every().day.at("14:00").do(run_daily_tasks)
     schedule.every().day.at("20:00").do(run_daily_tasks)
-    
-    schedule.every().day.at("13:20").do(run_daily_tasks)
 
     while True:
         schedule.run_pending()
